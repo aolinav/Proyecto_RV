032d216c5371fa3ea6c86e3326424b3d0e4a2134
Alba Oliver <aolinav@epsg.upv.es>
2023-11-09 17:36:47 +0100

    boton

---
+++ Assets/scripts/volver.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class volver : MonoBehaviour
+{
+
+    public void menu_principal()
+    {
+        SceneManager.LoadScene("video");
+    }
+}
