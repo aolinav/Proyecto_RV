7406448d9e1057f3487869734290ff70c1bfd74e
Daniel Benavides Belmonte <ruxzfly@gmail.com>
2023-11-15 15:54:55 +0100

    Añadida luna con rotacion y objetos orbitandno

--- Assets/scripts/rotar.cs
+++ Assets/scripts/rotar.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class rotar : MonoBehaviour
 {
     public float velocidadDeRotacion = 0.5f;
+    public float velocidadRotacionLuna = 0.1f;
     public Transform pivote;
+    public List<Transform> objetosArotar;
 
+    private void Start()
+    {
+        // Limpiar la lista actual
+        objetosArotar.Clear();
+
+        // Obtener todos los objetos hijos de la luna
+        foreach (Transform hijo in pivote.transform)
+        {
+            objetosArotar.Add(hijo);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        this.transform.RotateAround(pivote.transform.position, Vector3.up, velocidadDeRotacion);
+        foreach (Transform objeto in objetosArotar)
+        {
+            objeto.transform.RotateAround(pivote.transform.position, Vector3.up, velocidadDeRotacion);
+        }
+        
+    }
+
+    void RotarLuna()
+    {
+        // Rotar la luna sobre su propio eje
+        pivote.transform.Rotate(Vector3.up, velocidadRotacionLuna * Time.deltaTime);
     }
 }
