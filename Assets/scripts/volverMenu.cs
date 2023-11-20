bf843b84df228d57b3d392b8cf4d3988e8d23717
Alba Oliver <aolinav@epsg.upv.es>
2023-11-20 16:11:50 +0100

    botones volver

---
+++ Assets/scripts/volverMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class volverMenu : MonoBehaviour
+{
+   public void menu_principal()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
